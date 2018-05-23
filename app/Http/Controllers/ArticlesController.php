bf13234867efeb0dcafcaa46767fb16aed59592d
zloyleva <zloyleva@gmail.com>
2018-05-23 22:06:40 +0300

    add Artiles + resource

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ArticleResource;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    //
+    public function index(Article $article){
+//        return $article->paginate(5);
+        return ArticleResource::collection($article->paginate(5));
+    }
+
+    public function show($id, Article $article){
+        return new ArticleResource($article->findOrFail($id));
+    }
+
+    public function delete($id, Article $article){
+        $item = $article->findOrFail($id);
+
+        if($item->delete()){
+            return response([
+                'message' => 'was deleted'
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show')->where('id', '[0-9]+');
+Route::delete('articles/{id}', 'ArticlesController@delete')->where('id', '[0-9]+');
