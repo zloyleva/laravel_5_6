d6d8e5334f96d70da22cad57d7e351d64ec51b7a
zloyleva <zloyleva@gmail.com>
2018-06-28 01:05:04 +0300

    create folder for models

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,10 +15,5 @@ class UsersTableSeeder extends Seeder
         User::truncate();
         factory(User::class)->create(['name'=>'admin', 'email'=>'admin@test.com','password' => bcrypt('123456')]);
         factory(User::class,10)->create();
-
-        factory(User::class)->create([
-            'email' => 'admin@test.com',
-            'password' => bcrypt('123456'),
-        ]);
     }
 }
